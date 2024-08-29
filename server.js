b15eb83476644561337a2b4971a056685cd6c130
fr <fazlerabby122@gmail.com>
2024-08-29 20:13:21 +0600

    cors fix

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 const app = express();
-app.use(cors({
-    // origin:"http://localhost:5173",
-    origin:"https://hotel-booking-rho-one.vercel.app",
-    // origin:"https://hotel-booking-3iquqnytm-fazle122s-projects.vercel.app",
-    methods:['GET','POST','PUT','DELETE']
-}));
-// app.use('*', cors());
+// app.use(cors({
+//     // origin:"http://localhost:5173",
+//     origin:"https://hotel-booking-rho-one.vercel.app",
+//     // origin:"https://hotel-booking-3iquqnytm-fazle122s-projects.vercel.app",
+//     methods:['GET','POST','PUT','DELETE']
+// }));
+app.use('*', cors());
 app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
