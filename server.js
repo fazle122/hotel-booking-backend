6c19ca7f533d365fa4b9c0fafa59f942efdd1e7c
fr <fazlerabby122@gmail.com>
2024-08-29 20:10:47 +0600

    origin fix

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 const app = express();
-// const app = express();
-// // app.use(cors({
-// //     origin:"http://localhost:5173",
-// //     // origin:"https://hotel-booking-3iquqnytm-fazle122s-projects.vercel.app",
-// //     methods:['GET','POST','PUT','DELETE']
-// // }));
-app.use('*', cors());
+app.use(cors({
+    // origin:"http://localhost:5173",
+    origin:"https://hotel-booking-rho-one.vercel.app",
+    // origin:"https://hotel-booking-3iquqnytm-fazle122s-projects.vercel.app",
+    methods:['GET','POST','PUT','DELETE']
+}));
+// app.use('*', cors());
 app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
@@ -52,3 +52,6 @@ app.use(errorHandler);
 const server = app.listen(port, () =>{
     console.log(`server is running ${port} in ${process.env.NODE_ENV} mode`)
 })
+
+// https://hotel-booking-backend-kohl.vercel.app/api/cabins
+// https://hotel-booking-rho-one.vercel.app/api/cabins?pageNo=1&sortBy=-startDate
