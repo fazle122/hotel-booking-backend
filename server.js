3019da2bf4cdba084d2745cd646b0ab1018c78e0
fr <fazlerabby122@gmail.com>
2024-08-29 19:44:36 +0600

    cord issue update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import paymentRoutes from './routes/paymentRoute.js'
 import settingsRoutes from './routes/settingsRoute.js'
 import cookieParser   from 'cookie-parser';
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import cors from 'cors';
 
 
 // dotenv.config({path:"backend/.env"});
@@ -18,7 +19,13 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 const app = express();
-// app.use(express.json());
+// const app = express();
+// // app.use(cors({
+// //     origin:"http://localhost:5173",
+// //     // origin:"https://hotel-booking-3iquqnytm-fazle122s-projects.vercel.app",
+// //     methods:['GET','POST','PUT','DELETE']
+// // }));
+app.use('*', cors());
 app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
