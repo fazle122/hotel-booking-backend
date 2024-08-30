1d74d99eb893ef3f75eab47763d87cd2a98d3adb
fr <fazlerabby122@gmail.com>
2024-08-30 10:31:24 +0600

    cors all

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,16 +3,21 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (res,userId) => {
     const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:'30d'})
+    console.log('token: ',token);
     res.cookie(
         'hmjwt',
         token,
         {
             httpOnly:true,
-            secure:process.env.NODE_ENV !== 'development',
-            sameSite:'strict',
+            // secure:process.env.NODE_ENV !== 'development',
+            // sameSite:'strict',
+
+            secure:true,
+            sameSite:'none',
             maxAge:30*24*60*60*100
         }
     )
 }
 
 export default generateToken;
+
