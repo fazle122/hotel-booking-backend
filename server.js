9285bf95443478a21ffc668a2021c3635d8a6eed
fr <fazlerabby122@gmail.com>
2024-08-30 16:03:47 +0600

    cors update 3

--- server.js
+++ server.js
@@ -19,34 +19,34 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 const app = express();
-// app.use(cors({
-//     origin:"http://localhost:5173",
-//     // origin:"https://hotel-booking-rho-one.vercel.app",
-//     // origin:"https://hotel-booking-3iquqnytm-fazle122s-projects.vercel.app",
-//     methods:['GET','POST','PUT','DELETE']
-// }));
 app.use(
     cors({
         origin: [
             "http://localhost:5173",
             "https://hotel-booking-rho-one.vercel.app"
-            // "http://localhost:3000",
-            // "http://localhost:3001",
-            // "http://localhost:3002",
-            // "https://code-daily.vercel.app",
-            // "https://code-daily-sachanayush47.vercel.app",
-            // "http://192.168.43.197:3000",
         ],
-        methods:['GET','POST','PUT','DELETE']
-        // credentials: true,
+        methods:['GET','POST','PUT','DELETE'],
+        credentials: true,
         // allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 // app.use('*', cors());
-// app.use(cors({credentials:true}));
+
 app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
+
+
+
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  });
+
 
 
 // import asyncHandler from "./middleware/asyncHandler.js";
