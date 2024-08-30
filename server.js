b0cc5fc1109c6d1162b362dfe7cb0c92a53dc9b2
fr <fazlerabby122@gmail.com>
2024-08-30 12:09:08 +0600

    cors update 2

--- server.js
+++ server.js
@@ -25,7 +25,24 @@ const app = express();
 //     // origin:"https://hotel-booking-3iquqnytm-fazle122s-projects.vercel.app",
 //     methods:['GET','POST','PUT','DELETE']
 // }));
-app.use('*', cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://hotel-booking-rho-one.vercel.app"
+            // "http://localhost:3000",
+            // "http://localhost:3001",
+            // "http://localhost:3002",
+            // "https://code-daily.vercel.app",
+            // "https://code-daily-sachanayush47.vercel.app",
+            // "http://192.168.43.197:3000",
+        ],
+        methods:['GET','POST','PUT','DELETE']
+        // credentials: true,
+        // allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+// app.use('*', cors());
 // app.use(cors({credentials:true}));
 app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({extended:true}))
